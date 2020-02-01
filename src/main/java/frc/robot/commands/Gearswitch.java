97bd31c5b20239b584d403fe7554f8b60ffe32fd
GPIOctapus <sdj2016@gmail.com>
2020-02-01 14:47:12 -0500

    oops, forgot gearswitch

---
+++ src/main/java/frc/robot/commands/Gearswitch.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.Pneumatics;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Gearswitch extends CommandBase
+{
+    private final Pneumatics m_pneumatics;
+
+    public Gearswitch(Pneumatics subsystem)
+    {
+        m_pneumatics = subsystem;
+
+        addRequirements(m_pneumatics);
+    }
+
+    @Override
+    public void initialize()
+    {
+        m_pneumatics.gearSwitch();
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return true;
+    }
+
+}
