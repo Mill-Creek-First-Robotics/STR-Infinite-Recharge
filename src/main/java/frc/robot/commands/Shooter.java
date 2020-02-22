bc9f87584eb9299a9fa5aec38b138b85b3df21db
SteelTalonRoboticsLaptop <frc6910@gmail.com>
2020-02-22 11:27:25 -0500

    Added Ball Holder Support

--- src/main/java/frc/robot/commands/Shooter.java
+++ src/main/java/frc/robot/commands/Shooter.java
@@ -1,28 +1,29 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.BallShooter;
+import frc.robot.subsystems.Pneumatics;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Shooter extends CommandBase
-{
+public class Shooter extends CommandBase {
     private final BallShooter m_ballShooter;
+    private final Pneumatics m_pneumatics;
 
-    public Shooter(BallShooter subsystem)
-    {
-        m_ballShooter = subsystem;
+    public Shooter(BallShooter bsSubsystem, Pneumatics pnSubsystem) {
+        m_ballShooter = bsSubsystem;
+        m_pneumatics = pnSubsystem;
 
         addRequirements(m_ballShooter);
+        addRequirements(m_pneumatics);
+
     }
 
     @Override
-    public void initialize()
-    {
+    public void initialize() {
         m_ballShooter.pew();
     }
 
     @Override
-    public boolean isFinished()
-    {
+    public boolean isFinished() {
         return true;
     }
 
